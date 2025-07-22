28fc9ab01783b9ae3fac3ccaf29f14fdd4ae26ef
emisanzmor <emisn1501@gmail.com>
2025-07-21 20:06:55 -0600

    First commit

---
+++ moodiq/moodiqApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodiqApp.swift
+//  moodiq
+//
+//  Created by Emi Sanzmor on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct moodiqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moodiqTests/moodiqTests.swift
@@ -0,0 +1,17 @@
+//
+//  moodiqTests.swift
+//  moodiqTests
+//
+//  Created by Emi Sanzmor on 21/07/25.
+//
+
+import Testing
+@testable import moodiq
+
+struct moodiqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
